6e07d4c249e8c833022bbcf6a6d47ae13147cc06
Kimmi Lee <lee.kimmixq@gmail.com>
2023-04-23 15:40:21 +0800

    feat: setup lint, mui, react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+import { Layout } from './components/Layout'
+import { Home } from './pages/Home'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout isLoggedIn={false} />}>
+          <Route index element={<Home />} />
+          <Route path="*" element={<Home />} />
+        </Route>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,5 @@
+import Typography from '@mui/material/Typography'
+
+export function Home() {
+  return <Typography>Login</Typography>
+}
